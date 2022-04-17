88ca4959802b29de2ebf29140cf71602bb6c2a8f
harryhan1989 <harryhan1989@hotmail.com>
2022-04-17 17:58:13 +0800

    容器重新编译，添加高可用模式，即：多fe模式

--- startup.sh
+++ startup.sh
@@ -6,15 +6,31 @@ initFe(){
     echo "start starrocks fe"
     [ -d "${FE_META}" ] || mkdir -p ${FE_META}
     sed -i "s|^[[:space:]]*\$LIMIT \$JAVA \$JAVA_OPTS org.apache.starrocks.PaloFe.*|    exec \$LIMIT \$JAVA \$JAVA_OPTS org.apache.starrocks.PaloFe \${HELPER} \"\$@\"|g" ${STARROCKS_HOME}/fe/bin/start_fe.sh
+    sed -i "s@^priority_networks.*@@g" ${STARROCKS_HOME}/fe/conf/fe.conf
+    sed -i "s@^meta_dir.*@@g" ${STARROCKS_HOME}/fe/conf/fe.conf
     echo "priority_networks = ${FE_NETWORK}" >> ${STARROCKS_HOME}/fe/conf/fe.conf
     echo "meta_dir = ${FE_META}" >> ${STARROCKS_HOME}/fe/conf/fe.conf
-    exec bash ${STARROCKS_HOME}/fe/bin/start_fe.sh
+
+    #ENV MASTER_FE_HOST, FE_HOST, FE_TYPE(follower,observer), FOR HA
+    if [[ -n ${MASTER_FE_HOST} ]];
+    then
+        COUNT=$(mysql -h${MASTER_FE_HOST} -P9030 -uroot -e"SHOW PROC '/frontends'\G;" | grep ${FE_HOST} | wc -l)
+        if [ $COUNT -lt 1  ];
+        then
+            mysql -h${MASTER_FE_HOST} -P9030 -uroot -e"ALTER SYSTEM ADD ${FE_TYPE} \"${FE_HOST}:9010\";"
+        fi
+        exec bash ${STARROCKS_HOME}/fe/bin/start_fe.sh --helper "${MASTER_FE_HOST}:9010"
+    else
+        exec bash ${STARROCKS_HOME}/fe/bin/start_fe.sh
+    fi
 }
 
 initBe(){
     echo "start starrocks be"
     [ -d "${BE_DATA}" ] || mkdir -p ${BE_DATA}
-    sed -i "s@^storage_root_path.*@storage_root_path = ${STARROCKS_HOME}/be/data@g" ${STARROCKS_HOME}/be/conf/be.conf
+    #sed -i "s@^storage_root_path.*@storage_root_path = ${STARROCKS_HOME}/be/data@g" ${STARROCKS_HOME}/be/conf/be.conf
+    sed -i "s@^priority_networks.*@@g" ${STARROCKS_HOME}/be/conf/be.conf
+    sed -i "s@^storage_root_path.*@@g" ${STARROCKS_HOME}/be/conf/be.conf
     echo "priority_networks = ${BE_NETWORK}" >> ${STARROCKS_HOME}/be/conf/be.conf
     echo "storage_root_path = ${BE_DATA}" >> ${STARROCKS_HOME}/be/conf/be.conf
     sed -i "s|^[[:space:]]*\$LIMIT \${STARROCKS_HOME}/lib/palo_be.*|    exec \$LIMIT \${STARROCKS_HOME}/lib/palo_be \"\$@\"|g" ${STARROCKS_HOME}/be/bin/start_be.sh
